d425b1ed43893869934641e48730b97ad75c9c86
Lloyd Atkinson <lloydjatkinson@gmail.com>
2019-03-24 22:34:01 +0000

    Implement temperature monitor

--- RaspberryPiDotNetCoreAzureIoT/RaspberryPiSensorNode/Temperature/CpuTemperatureMonitor.cs
+++ RaspberryPiDotNetCoreAzureIoT/RaspberryPiSensorNode/Temperature/CpuTemperatureMonitor.cs
@@ -1,16 +1,62 @@
-﻿using System.Threading;
+﻿using System;
+using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
+
+using Microsoft.Azure.Devices.Client;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Options;
+
+using Newtonsoft.Json;
+using Newtonsoft.Json.Serialization;
+
+using RaspberryPiSensorNode.Configuration;
 
 namespace RaspberryPiSensorNode.Temperature
 {
     public class CpuTemperatureMonitor : ICpuTemperatureMonitor
     {
+        private readonly ILogger<CpuTemperatureMonitor> _logger;
+        private readonly IOptions<AzureIoTHubConfiguration> _azureIoTHubConnectionConfiguration; //TODO: Look into removing dependency on IOptions in here.
+        private readonly ITemperatureReader _temperatureReader;
+        private readonly DeviceClient _deviceClient;
+
+
+        public CpuTemperatureMonitor(ILogger<CpuTemperatureMonitor> logger, IOptions<AzureIoTHubConfiguration> azureIoTHubConnectionConfiguration, ITemperatureReader temperatureReader)
+        {
+            _logger = logger;
+            _azureIoTHubConnectionConfiguration = azureIoTHubConnectionConfiguration;
+            _temperatureReader = temperatureReader;
+            _deviceClient = DeviceClient.CreateFromConnectionString(_azureIoTHubConnectionConfiguration.Value.ConnectionString, TransportType.Mqtt);
+        }
+
         public async Task Run(CancellationToken cancellationToken)
         {
             while (!cancellationToken.IsCancellationRequested)
             {
-                System.Console.WriteLine("test");
-                await Task.Delay(1000);
+                try
+                {
+                    _logger.LogInformation("Reading temperature...");
+                    var reading = _temperatureReader.Read();
+                    //var reading = new TemperatureReading(true, 20d);
+                    _logger.LogInformation($"Temperature: {reading.Celcius}");
+
+                    var message = new Message(
+                        Encoding.ASCII.GetBytes(
+                            JsonConvert.SerializeObject(
+                                reading, new JsonSerializerSettings() { ContractResolver = new CamelCasePropertyNamesContractResolver() })));
+
+                    message.Properties.Add("couldRead", reading.CouldRead ? "true" : "false");
+
+                    _logger.LogInformation("Sending reading to Azure IoT Hub.");
+                    await _deviceClient.SendEventAsync(message, cancellationToken);
+                }
+                catch (Exception exception)
+                {
+                    _logger.LogError(exception, "Unable to send CPU temperature to Azure IoT Hub.");
+                }
+
+                await Task.Delay(5000); //TOOD: Use a Timer instead.
             }
         }
     }
